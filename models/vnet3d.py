+ fixed labels bug + normalization + pred on training images

models/vnet3d.py
@@ -160,8 +160,7 @@ class VXNet3D(nn.Module):
         self.up_block_1   = UpConvBlock2b(out_channels=channels * 4, undersampling_factor=4)
         self.cat_block_1  = CatBlock()
 
-        self.out_conv     = FinalConv(num_outs=num_outs)
-
+        self.out_conv     = FinalConv(num_outs=num_outs, out_channels=channels * 2)
 
     def forward(self, x):
 

semseg/data_loader.py
@@ -60,7 +60,7 @@ class Dataset3DFull(torch.utils.data.Dataset):
         return (inputs, labels)
 
     def normalize(self, inputs):
-        return inputs
+        return min_max_normalization(inputs)
 
     def perform_zero_pad(self, inputs, labels):
         inputs_padded = np.zeros(self.pad_ref)
@@ -69,3 +69,19 @@ class Dataset3DFull(torch.utils.data.Dataset):
         inputs_padded[:inputs.shape[0], :inputs.shape[1], :inputs.shape[2]] = inputs
         labels_padded[:labels.shape[0], :labels.shape[1], :labels.shape[2]] = labels
         return (inputs_padded, labels_padded)
+
+
+def GetDataLoader3D(config: SemSegConfig) -> torch.utils.data.DataLoader:
+    print('Building Training Set Loader...')
+    train = Dataset3DFull(config.train_images, config.train_labels,
+                          augmentation=config.augmentation, do_normalize=config.do_normalize,
+                          zero_pad=config.zero_pad, pad_ref=config.pad_ref)
+
+    train_data = torch.utils.data.DataLoader(train, batch_size=config.batch_size, shuffle=True,
+                                             num_workers=config.num_workers)
+    print('Training Loader built!')
+    return train_data
+
+
+def min_max_normalization(input):
+    return (input - input.min()) / (input.max() - input.min())

semseg/loss.py
@@ -19,7 +19,7 @@ def one_hot_encode(label, num_classes):
     :param num_classes: K classes
     :return: label_ohe, Tensor of shape BxKxHxW or BxKxDxHxW
     """
-    assert(len(label.shape) == 3 or len(label.shape) == 4, 'Invalid Label Shape {}'.format(label.shape))
+    assert len(label.shape) == 3 or len(label.shape) == 4, 'Invalid Label Shape {}'.format(label.shape)
     if len(label.shape) == 3:
         label_ohe = torch.zeros((label.shape[0], num_classes, label.shape[1], label.shape[2]))
     elif len(label.shape) == 4:
@@ -58,7 +58,7 @@ def dice_n_classes(outputs, labels, do_one_hot=False, get_list=False, device=Non
     if get_list:
         return dices
     else:
-        return sum(dices) / num_classes
+        return sum(dices) / (num_classes-1)
 
 
 def get_multi_dice_loss(outputs, labels, device=None):

train.py
@@ -15,7 +15,8 @@ import torch.optim as optim
 # Local Imports
 ##########################
 from config import *
-from semseg.utils import GetDataLoader3D, train_model
+from semseg.utils import train_model
+from semseg.data_loader import GetDataLoader3D
 from models.vnet3d import VXNet3D
 
 ##########################
@@ -48,7 +49,7 @@ iterable_data_loader = iter(train_data_loader_3D)
 inputs,labels = next(iterable_data_loader)
 print("Shape of Batch: [input {}] [label {}]".format(inputs.shape, labels.shape))
 
-net = VXNet3D(num_outs=3)
+net = VXNet3D(num_outs=config.num_outs, channels=8)
 outputs = net(inputs)
 print("Shape of Output: [output {}]".format(outputs.shape))
 
@@ -62,3 +63,5 @@ net = net.to(cuda_dev)
 
 net = train_model(net, optimizer, train_data_loader_3D,
                   config, device=cuda_dev, logs_folder=logs_folder)
+
+torch.save(net,os.path.join(logs_folder,"model.pt"))