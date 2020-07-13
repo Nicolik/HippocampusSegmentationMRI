6cc66b6a8ac7f061650acbd9d8a06e8a90845ca5
Nicolik <33285381+Nicolik@users.noreply.github.com>
2020-07-13 16:49:24 +0200

    + paths check

--- config/paths.py
+++ config/paths.py
@@ -9,12 +9,16 @@ train_images = os.listdir(train_images_folder)
 train_labels = os.listdir(train_labels_folder)
 
 train_images = [train_image for train_image in train_images
-                if train_image.endswith(".nii.gz")]
+                if train_image.endswith(".nii.gz") and not train_image.startswith('.')]
 train_labels = [train_label for train_label in train_labels
-                if train_label.endswith(".nii.gz")]
+                if train_label.endswith(".nii.gz") and not train_label.startswith('.')]
 test_images_folder = os.path.join(base_dataset_dir, "imagesTs")
 test_images = os.listdir(test_images_folder)
 test_prediction_folder = os.path.join(base_dataset_dir, "predTs")
+
+test_images = [test_image for test_image in test_images
+               if test_image.endswith(".nii.gz") and not test_image.startswith('.')]
+
 labels_names = {
    "0": "background",
    "1": "Anterior",
